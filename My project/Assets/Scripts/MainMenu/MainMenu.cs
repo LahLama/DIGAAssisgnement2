a38e1c244c283ce58be1c60dbd9f164e37a433b1
Premise18 <promisengeno@gmail.com>
2025-05-18 13:03:35 +0200

    Settings menu UI and codes

--- My project/Assets/Scripts/MainMenu/MainMenu.cs
+++ My project/Assets/Scripts/MainMenu/MainMenu.cs
@@ -1,12 +1,17 @@
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;   
 
 public class MainMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
 
+    public Slider musicVolumeSlider;
+    public Slider sfxVolumeSlider;
+
     private void Start()
     {
+        LoadVolume();
         MusicManager.Instance.PlayMusic("Main Menu");
     }
 
@@ -29,5 +34,20 @@ public class MainMenu : MonoBehaviour
     public void UpdateSFXVolume(float volume)
     {
         audioMixer.SetFloat("SFXVolume", volume);
+    }
+
+    public void SaveVolume()
+    {
+        audioMixer.GetFloat("MusicVolume", out float musicVolume);
+        PlayerPrefs.SetFloat("MusicVolume", musicVolume);
+
+        audioMixer.GetFloat("SFXVolume", out float sfxVolume);
+        PlayerPrefs.SetFloat("SFXVolume", sfxVolume);
+    }
+
+    public void LoadVolume()
+    {
+        musicVolumeSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+        sfxVolumeSlider.value = PlayerPrefs.GetFloat("SFXVolume");
     }
 }
