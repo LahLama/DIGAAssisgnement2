70cdabcdc5b4922c403efb557e97d81a6dc114bd
Premise18 <promisengeno@gmail.com>
2025-05-18 10:11:54 +0200

    Main Menu Canvas

--- My project/Assets/Scripts/MainMenu/MainMenu.cs
+++ My project/Assets/Scripts/MainMenu/MainMenu.cs
@@ -1,11 +1,19 @@
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioMixer audioMixer;
+
+    private void Start()
+    {
+        MusicManager.Instance.PlayMusic("Main Menu");
+    }
 
     public void ButtonStartGame()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("True James Room");
+        MusicManager.Instance.PlayMusic("True James Room");
     }
 
     public void ButtonExitGame()
@@ -13,4 +21,13 @@ public class MainMenu : MonoBehaviour
         Application.Quit();
     }
 
+    public void UpdateMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("MusicVolume", volume);
+    }
+
+    public void UpdateSFXVolume(float volume)
+    {
+        audioMixer.SetFloat("SFXVolume", volume);
+    }
 }
